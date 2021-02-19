e88a03fa996d23d64c961fc6426d1cddf231ce2e
LeoH1310 <44778508+LeoH1310@users.noreply.github.com>
2021-02-19 23:19:25 +0100

    staffel nummer aus dateiname auslesen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace HeRoSorter
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -29,13 +29,87 @@ namespace HeRoSorter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.rbFilm = new System.Windows.Forms.RadioButton();
+            this.rbSerie = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(633, 77);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 50);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Pfad..";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Enabled = false;
+            this.tbPath.Location = new System.Drawing.Point(53, 91);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(500, 22);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(53, 181);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(100, 40);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "HeRoSort!";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // rbFilm
+            // 
+            this.rbFilm.AutoSize = true;
+            this.rbFilm.Location = new System.Drawing.Point(121, 135);
+            this.rbFilm.Name = "rbFilm";
+            this.rbFilm.Size = new System.Drawing.Size(54, 21);
+            this.rbFilm.TabIndex = 3;
+            this.rbFilm.Text = "Film";
+            this.rbFilm.UseVisualStyleBackColor = true;
+            // 
+            // rbSerie
+            // 
+            this.rbSerie.AutoSize = true;
+            this.rbSerie.Checked = true;
+            this.rbSerie.Location = new System.Drawing.Point(53, 135);
+            this.rbSerie.Name = "rbSerie";
+            this.rbSerie.Size = new System.Drawing.Size(62, 21);
+            this.rbSerie.TabIndex = 4;
+            this.rbSerie.TabStop = true;
+            this.rbSerie.Text = "Serie";
+            this.rbSerie.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(782, 450);
+            this.Controls.Add(this.rbSerie);
+            this.Controls.Add(this.rbFilm);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "MainForm";
+            this.Text = "HeRoSorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.RadioButton rbFilm;
+        private System.Windows.Forms.RadioButton rbSerie;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,72 @@ using System.Windows.Forms;
 
 namespace HeRoSorter
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private string folderPath;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            // Set the help text description for the FolderBrowserDialog.
+            folderBrowserDialog.Description = "Select the folder to be sorted.";
+
+            // Do not allow the user to create new files via the FolderBrowserDialog.
+            folderBrowserDialog.ShowNewFolderButton = false;
+
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog.SelectedPath;
+                tbPath.Text = folderPath;
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            // ToDo: Check input
+            string dirTo = folderPath + "\\HeRoSorted";
+            DirectoryInfo di = Directory.CreateDirectory(dirTo);
+
+            heroSort(folderPath, dirTo, rbSerie.Checked);
+
+        }
+
+        private void heroSort(string dirFrom, string dirTo, bool serie)
+        {
+            if (serie)
+            {
+                string[] files = Directory.GetFiles(dirFrom);
+                foreach (string f in files)
+                {
+                    int season = getSeasonFromName(f);
+                }
+            }
+        }
+
+        private int getSeasonFromName(string fileName)
+        {
+            for(int i = 0; i <= fileName.Length; i++)
+            {
+                string debug1 = fileName.Substring(i, 1).ToLower();
+                string debug2 = fileName.Substring(i + 1, 2).ToLower();
+                if (fileName.Substring(i, 1).ToLower() == "s"
+                        && fileName.Substring(i + 1, 2).All(char.IsDigit)
+                        && fileName.Substring(i + 3, 1).ToLower() == "e"
+                        && fileName.Substring(i + 4, 2).All(char.IsDigit))
+                {
+                    return Int32.Parse(fileName.Substring(i + 1, 2));
+                }
+            }
+            return -1;
         }
     }
 }
